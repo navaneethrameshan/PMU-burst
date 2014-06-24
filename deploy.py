229d2861514ec59cd6a62d018ce8b5d6fbf5a045
navaneeth <navaneeth.rameshan@gmail.com>
2014-06-24 23:27:42 +0200

    remote deploy spec CPU on LXC containers

--- deploy.py
+++ deploy.py
@@ -1,1 +1,50 @@
-__author__ = 'navaneeth'
+from fabric.api import *
+import json
+import urllib2
+import shelve
+import os
+import socket, paramiko
+import fabric.version
+nodes = ['10.0.3.27']
+
+
+if __name__ =='__main__':
+    path= os.path.join(os.path.dirname(__file__), 'node_list_shelf.db')
+    success_list = []
+    if(os.path.exists(path)):
+        os.remove(path)
+
+    print nodes
+
+
+# We can then specify host(s) and run the same commands across those systems
+env.user = 'ubuntu'
+env.password = 'ubuntu'
+env.warn_only = True
+
+
+def uptime():
+    if _is_host_up(env.host, int(env.port)) is True:
+        run("uptime")
+
+def run_spec():
+    if _is_host_up(env.host, int(env.port)) is True:
+        with cd("cpu2006-installed"):
+            run(". ./shrc; time bin/runspec --config nav-gcc43.cfg --noreportable --iterations=1 471.omnetpp > result-log", pty=False)
+
+
+def _is_host_up(host, port):
+    # Set the timeout
+    original_timeout = socket.getdefaulttimeout()
+    new_timeout = 10
+    socket.setdefaulttimeout(new_timeout)
+    host_status = False
+    try:
+        transport = paramiko.Transport((host, port))
+        host_status = True
+    except:
+        print('***Warning*** Host {host} on port {port} is down.'.format(
+            host=host, port=port)
+            )
+    socket.setdefaulttimeout(original_timeout)
+    return host_status
